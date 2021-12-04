61279da977698b20ed0ab2033bfb671b0bbc2a00
Bhargav <bhargav.g.912@gmail.com>
2021-12-04 21:25:25 +0530

    REFS: #2492 and Fixes: #2492

---
+++ ReactJs/Bhargav Gangadharani/Day3-4/practice/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import BookApp from "./Components/Book/BookApp";
+
+function App() {
+  return (
+    <div>
+      <BookApp></BookApp>
+    </div>
+  );
+}
+
+export default App;

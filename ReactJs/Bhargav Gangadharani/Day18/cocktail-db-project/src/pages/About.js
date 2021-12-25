40a4d8ca4d91106d57063b5e30674f075fc58c4d
Bhargav <bhargav.g.912@gmail.com>
2021-12-25 20:18:06 +0530

    REFS: #2692 and Fixes: #2692

---
+++ ReactJs/Bhargav Gangadharani/Day18/cocktail-db-project/src/pages/About.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+        elementum sit amet augue in interdum. Aenean in augue sit amet dolor
+        gravida egestas. Vestibulum sagittis suscipit purus, id fringilla lectus
+        venenatis in. Curabitur facilisis urna arcu, quis mattis tellus aliquet
+        at. In neque nisi, mollis non tincidunt non, aliquam ac enim. Sed
+        tincidunt magna urna, ornare imperdiet dolor sagittis in. Suspendisse
+        iaculis et sapien at hendrerit. Morbi rutrum quam id sapien elementum
+        tempor. Morbi turpis ipsum, tincidunt ac cursus vel, vestibulum vel
+        mauris. Donec eget ipsum sodales sapien dignissim aliquam a a elit.
+        Praesent in mauris sed tellus porta eleifend. In hac habitasse platea
+        dictumst. Quisque vel convallis turpis, quis euismod metus. Fusce eu
+        varius arcu. Nunc auctor mauris et purus tincidunt tristique a sit amet
+        urna. Aliquam volutpat rhoncus purus, ut tincidunt sem hendrerit id.
+        Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. In rhoncus, urna at pretium consequat, ipsum elit pretium
+        sem, et placerat sapien arcu quis ligula. Phasellus enim nunc,
+        scelerisque vitae metus sit amet, congue semper lectus. Fusce non lorem
+        sem. Donec dictum vulputate leo, at maximus sapien mollis facilisis.
+      </p>
+    </section>
+  );
+}
